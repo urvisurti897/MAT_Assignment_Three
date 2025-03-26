f10981f0d931977dccbdf6d02a2a46252975de05
Urvi <urvisurti897@gmail.com>
2025-03-26 15:05:58 -0400

    calculateCanvasDiagonal.test.ts: Adding test for validating negative number

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -2,13 +2,19 @@ import calculateCanvasDiagonal from './calculateCanvasDiagonal';
 
 describe('calculateCanvasDiagonal', () => {
   test('should calculate diagonal correctly for positive values', () => {
-    const result = calculateCanvasDiagonal("2", "4");
+    const result = calculateCanvasDiagonal(2, 4);
 
     expect(result).toEqual(12);
   });
 
-  test.todo("should throw an error if length is negative");
-  test.todo("should throw an error if width is negative");
+  test("should throw an error if length is negative", () => {
+    expect(() => calculateCanvasDiagonal(-2, 4)).toThrow("Length should be positive");
+  });
+
+  test("should throw an error if width is negative", () => {
+    expect(() => calculateCanvasDiagonal(2, -4)).toThrow("Width should be positive");
+  });
+
   test.todo("should throw an error if length is zero");
   test.todo("should throw an error if width is zero");
   test.todo("should throw an error if length is non-numeric");
