50375a5fb229f9be42c6d7d214a3f8f28d2b869b
Urvi <urvisurti897@gmail.com>
2025-03-26 14:52:26 -0400

    calculateCanvasDiagonal.test.ts: Adding todo list

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -2,24 +2,15 @@ import calculateCanvasDiagonal from './calculateCanvasDiagonal';
 
 describe('calculateCanvasDiagonal', () => {
   test('should calculate diagonal correctly for positive values', () => {
-    expect(calculateCanvasDiagonal('3', '4')).toBeCloseTo(5);
-    expect(calculateCanvasDiagonal('6', '8')).toBeCloseTo(10);
-    expect(calculateCanvasDiagonal('5', '12')).toBeCloseTo(13);
+    const result = calculateCanvasDiagonal("2", "4");
+
+    expect(result).toEqual(12);
   });
 
-  test('should return 0 for length and width of 0', () => {
-    expect(calculateCanvasDiagonal('0', '0')).toBe(0);
-  });
-
-  test('should handle negative values correctly', () => {
-    expect(calculateCanvasDiagonal('-3', '4')).toBeCloseTo(5);
-    expect(calculateCanvasDiagonal('3', '-4')).toBeCloseTo(5);
-    expect(calculateCanvasDiagonal('-3', '-4')).toBeCloseTo(5);
-  });
-
-  test('should handle non-numeric strings by returning NaN', () => {
-    expect(calculateCanvasDiagonal('a', '4')).toBeNaN();
-    expect(calculateCanvasDiagonal('3', 'b')).toBeNaN();
-    expect(calculateCanvasDiagonal('a', 'b')).toBeNaN();
-  });
+  test.todo("should throw an error if length is negative");
+  test.todo("should throw an error if width is negative");
+  test.todo("should throw an error if length is zero");
+  test.todo("should throw an error if width is zero");
+  test.todo("should throw an error if length is non-numeric");
+  test.todo("should throw an error if width is non-numeric");
 });
